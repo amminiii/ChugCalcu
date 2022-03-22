4d03ac2f74c5407a0c114e9ba76f4150cc584a52
amminiii <chugiyenammini@gmail.com>
2022-03-22 12:57:30 +0530

    Update maincalc.py

--- maincalc.py
+++ maincalc.py
@@ -20,7 +20,7 @@ def clear():
         os.system("clear")
 
 #lev calc
-def price_point(entry,leverage,rr,label):
+def short(entry,leverage,rr,label):
     pop  = round(float("{:.2f}".format(rr/leverage)), 2)
     if label == 'TP':
         out = entry + ((entry*float(pop))/100)
@@ -30,30 +30,68 @@ def price_point(entry,leverage,rr,label):
 
     return out
 
+def short(entry,leverage,rr,label):
+    pop  = round(float("{:.2f}".format(rr/leverage)), 2)
+    if label == 'TP':
+        out = entry - ((entry*float(pop))/100)
+    elif label == 'SL' :
+        out = entry + ((entry*float(pop))/100)
+    
+
+    return out
+
+
 def process():
     leverage = int(os.environ.get("DEF_LEV"))
     tp = float(os.environ.get("REWARD"))
     sl = float(os.environ.get("RISK"))
     entry = float(input("Enter entry price - "))
+    choice = input("Long or Short (l/s) - ")
 
-    print(f"""
-    LEVERAGE - {leverage}
-    
-    TP - {price_point(entry,leverage,tp,'TP')}
-    SL - {price_point(entry,leverage,tp,'SL')}
+    if choice == 'l':
+        print(f"""
+        LEVERAGE - {leverage}
+        Direction - LONG
+        
+        TP - {short(entry,leverage,tp,'TP')}
+        SL - {short(entry,leverage,sl,'SL')}
 
-    """)
+        """)
 
-    print("Press anything to copy tp.",end='')
-    input()
-    pyperclip.copy(price_point(entry,leverage,tp,'TP'))
-    print("Copied !")
-    input()
-    print("Press anything to copy sl.",end='')
-    input()
-    pyperclip.copy(price_point(entry,leverage,tp,'SL'))
-    print("Copied !")
-    input()
+        print("Press anything to copy tp.",end='')
+        input()
+        pyperclip.copy(short(entry,leverage,tp,'TP'))
+        print("Copied !")
+        input()
+        print("Press anything to copy sl.",end='')
+        input()
+        pyperclip.copy(short(entry,leverage,tp,'SL'))
+        print("Copied !")
+        input()
+
+    elif choice == 's':
+        print(f"""
+        LEVERAGE - {leverage}
+        Direction - SHORT
+        
+        TP - {short(entry,leverage,tp,'TP')}
+        SL - {short(entry,leverage,sl,'SL')}
+
+        """)
+
+        print("Press anything to copy tp.",end='')
+        input()
+        pyperclip.copy(short(entry,leverage,tp,'TP'))
+        print("Copied !")
+        input()
+        print("Press anything to copy sl.",end='')
+        input()
+        pyperclip.copy(short(entry,leverage,tp,'SL'))
+        print("Copied !")
+        input()
+
+
+
 
 while True:
     clear()
